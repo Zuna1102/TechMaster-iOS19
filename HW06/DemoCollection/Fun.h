21c1888c51101577963e1ed937c9e98ed039392f
ZUNA <vh.trungkien@gmail.com>
2013-12-06 01:01:17 +0700

    Update BT06

---
+++ HW06/DemoCollection/Fun.h
@@ -0,0 +1,13 @@
+//
+//  Fun.h
+//  DemoCollection
+//
+//  Created by TechMaster on 12/3/13.
+//  Copyright (c) 2013 TechMaster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fun : NSObject
+@property (nonatomic, strong) NSString* name;
+@end
---
+++ HW06/DemoCollection/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoCollection
+//
+//  Created by TechMaster on 12/3/13.
+//  Copyright (c) 2013 TechMaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
