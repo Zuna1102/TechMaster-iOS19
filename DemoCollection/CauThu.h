75dc0a187389966e77bc2d79fbbc500e210cece3
ZUNA <vh.trungkien@gmail>
2013-12-06 00:37:22 +0700

    Update HW06

---
+++ DemoCollection/CauThu.h
@@ -0,0 +1,16 @@
+//
+//  CauThu.h
+//  DemoCollection
+//
+//  Created by ZUNA on 12/5/13.
+//  Copyright (c) 2013 TechMaster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CauThu : NSObject
+float giaiThua(int a);
+float toHop(int a,int b);
+float chinhHop(int a, int b);
+
+@end
---
+++ DemoCollection/Fun.m
@@ -0,0 +1,13 @@
+//
+//  Fun.m
+//  DemoCollection
+//
+//  Created by TechMaster on 12/3/13.
+//  Copyright (c) 2013 TechMaster. All rights reserved.
+//
+
+#import "Fun.h"
+
+@implementation Fun
+
+@end
